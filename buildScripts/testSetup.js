c7d4eca0e3e5b005159edf67b2f874c9305b0dd3
Lennaert Goris <lennaert.goris@gmail.com>
2017-04-25 22:23:11 +0200

    Module 10

---
+++ buildScripts/testSetup.js
@@ -0,0 +1,11 @@
+/**
+ * Created by len on 4/25/2017.
+ */
+// This file isn't transpiled, so must use CommonJS and ES5
+
+// Register babel to transpile before our tests run.
+require('babel-register')();
+
+// Disable webpack features that Mocha doesn't understand.
+require.extensions['.css'] = function() {};
+
